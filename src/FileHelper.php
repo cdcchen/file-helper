6a0ef02fca638539e7f88ebf2996d637e569ba83
cdcchen <cdcchen@gmail.com>
2016-04-28 11:35:09 +0800

    Add FileHelper, PathBuilder

---
+++ src/FileHelper.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chendong
+ * Date: 16/3/13
+ * Time: 12:29
+ */
+
+namespace cdcchen\helpers\file;
+
+
+class FileHelper
+{
+    private static $_mimeTypes = [];
+
+    /**
+     * Determines the MIME type of the specified file.
+     * This method will first try to determine the MIME type based on
+     * [finfo_open](http://php.net/manual/en/function.finfo-open.php). If the `fileinfo` extension is not installed,
+     * it will fall back to [[getMimeTypeByExtension()]] when `$checkExtension` is true.
+     * @param string $file the file name.
+     * @param string $magicFile name of the optional magic database file (or alias), usually something like `/path/to/magic.mime`.
+     * This will be passed as the second parameter to [finfo_open()](http://php.net/manual/en/function.finfo-open.php)
+     * when the `fileinfo` extension is installed. If the MIME type is being determined based via [[getMimeTypeByExtension()]]
+     * and this is null, it will use the file specified by [[mimeMagicFile]].
+     * @param boolean $checkExtension whether to use the file extension to determine the MIME type in case
+     * `finfo_open()` cannot determine it.
+     * @return string the MIME type (e.g. `text/plain`). Null is returned if the MIME type cannot be determined.
+     * @throws \Exception when the `fileinfo` PHP extension is not installed and `$checkExtension` is `false`.
+     */
+    public static function getMimeType($file, $magicFile = null, $checkExtension = true)
+    {
+        if (!extension_loaded('fileinfo')) {
+            if ($checkExtension) {
+                return static::getMimeTypeByExtension($file, $magicFile);
+            } else {
+                throw new \Exception('The fileinfo PHP extension is not installed.');
+            }
+        }
+
+        $info = finfo_open(FILEINFO_MIME_TYPE, $magicFile);
+        if ($info) {
+            $result = finfo_file($info, $file);
+            finfo_close($info);
+
+            if ($result !== false) {
+                return $result;
+            }
+        }
+
+        return $checkExtension ? static::getMimeTypeByExtension($file, $magicFile) : null;
+    }
+
+    /**
+     * Determines the MIME type based on the extension name of the specified file.
+     * This method will use a local map between extension names and MIME types.
+     * @param string $file the file name.
+     * @param string $magicFile the path (or alias) of the file that contains all available MIME type information.
+     * If this is not set, the file specified by [[mimeMagicFile]] will be used.
+     * @return string the MIME type. Null is returned if the MIME type cannot be determined.
+     */
+    public static function getMimeTypeByExtension($file, $magicFile = null)
+    {
+        $mimeTypes = static::loadMimeTypes($magicFile);
+
+        if (($ext = pathinfo($file, PATHINFO_EXTENSION)) !== '') {
+            $ext = strtolower($ext);
+            if (isset($mimeTypes[$ext])) {
+                return $mimeTypes[$ext];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Loads MIME types from the specified file.
+     * @param string $magicFile the path (or alias) of the file that contains all available MIME type information.
+     * If this is not set, the file specified by [[mimeMagicFile]] will be used.
+     * @return array the mapping from file extensions to MIME types
+     */
+    protected static function loadMimeTypes($magicFile)
+    {
+        if ($magicFile === null) {
+            $magicFile = __DIR__ . DIRECTORY_SEPARATOR . 'mimeTypes.php';
+        }
+
+        if (!isset(self::$_mimeTypes[$magicFile])) {
+            self::$_mimeTypes[$magicFile] = require($magicFile);
+        }
+
+        return self::$_mimeTypes[$magicFile];
+    }
+}
---
+++ src/PathBuilder.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chendong
+ * Date: 15/5/22
+ * Time: 上午11:35
+ */
+
+namespace cdcchen\helpers\file;
+
+
+class SimplePath
+{
+    protected $basePath;
+    protected $pathName;
+    protected $fileName;
+
+    protected static function placeHolders($key = null)
+    {
+        $places = [
+            '{year}' => date('Y'),
+            '{month}' => date('m'),
+            '{day}' => date('d'),
+            '{hour}' => date('H'),
+            '{minute}' => date('i'),
+            '{second}' => date('s'),
+            '{week}' => date('W'),
+            '{wday}' => date('w'),
+            '{timestamp}' => time(),
+            '{uniqid}' => uniqid(),
+        ];
+
+        return empty($key) ? $places : $places[$key];
+    }
+
+    public function __construct($basePath = '')
+    {
+        $this->basePath = $basePath;
+    }
+
+    public function buildPathName($pathFormat, $prefix = null, $suffix = null)
+    {
+        $places = self::placeHolders();
+        $pathName = str_replace(array_keys($places), array_values($places), trim($pathFormat, "/\\"));
+
+        $prefix = rtrim($prefix, "/\\");
+        if ($prefix) {
+            $pathName = $prefix . DIRECTORY_SEPARATOR . $pathName;
+        }
+
+        $suffix = ltrim($suffix, "/\\");
+        if ($suffix) {
+            $pathName .= DIRECTORY_SEPARATOR . $suffix;
+        }
+
+        $this->pathName = $pathName;
+        return $this;
+    }
+
+    public function createPath($mode = 0755, $recursive = true, $context = null)
+    {
+        $basePath = (stripos($this->pathName, '/') === 0) ? '' : (rtrim($this->basePath, '/') . DIRECTORY_SEPARATOR);
+        $path = $basePath . $this->pathName;
+        return $context ? mkdir($path, $mode, $recursive, $context) : mkdir($path, $mode, $recursive);
+    }
+
+    public function buildFileName($fileFormat, $extensionName = '', $includeDot = false)
+    {
+        $places = self::placeHolders();
+        $fileName = str_replace(array_keys($places), array_values($places), $fileFormat);
+        if ($extensionName) {
+            $fileName .= ($includeDot ? '' : '.') . $extensionName;
+        }
+
+        $this->fileName = $fileName;
+        return $this;
+    }
+
+    public function getFilePath()
+    {
+        $basePath = (stripos($this->pathName, '/') === 0) ? '' : (rtrim($this->basePath, '/') . DIRECTORY_SEPARATOR);
+        return $basePath . $this->pathName . DIRECTORY_SEPARATOR . $this->fileName;
+    }
+
+    public function getFileUrl($baseUrl = null)
+    {
+        $baseUrl = $baseUrl ? rtrim($baseUrl, '/') . '/' : '';
+        $basePath = (stripos($this->pathName, '/') === 0) ? '' : (rtrim($this->basePath, '/') . DIRECTORY_SEPARATOR);
+        $path = ltrim(str_replace("\\", '/', $basePath . $this->pathName), '/') . '/';
+
+        return $baseUrl . $path . $this->fileName;
+    }
+}
